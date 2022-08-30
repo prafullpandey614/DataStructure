4cc218d2d2ff0bf10007d6d0e96da6bde5843135
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2022-08-30 23:20:43 +0530

    Create Rotate_Image_Matrix.cpp

---
+++ Array/Rotate_Image_Matrix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        for(int i  = 0; i < matrix.size();++i){
+            for(int j =0 ; j <=i;++j){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        
+        for(int x = 0 ; x <matrix.size();++x){
+            reverse(matrix[x].begin(),matrix[x].end());
+        }
+        
+    }
+};
