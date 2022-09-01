b5e44130276be51b3bb7ef81d02dd59a7d7e0ee5
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2022-09-01 22:08:42 +0530

    Awesome Simple DFS Traversal Leetcode

---
+++ Binary Tree/Count_Good_Nodes.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int count = 0;
+    void dfs(TreeNode* node , int prev ){
+        if(node==NULL ){
+            return ;
+        }
+        if(node->val>= prev){
+            count++;
+            prev = node->val ;        // Just Keep Track of Highest Value on each Node
+        }
+            dfs(node->right,prev);
+            dfs(node->left,prev);
+        
+    }
+    
+    int goodNodes(TreeNode* root) {
+        count = 1;
+        dfs(root->left,root->val);
+        dfs(root->right,root->val);
+        return count  ;
+    }
+};
