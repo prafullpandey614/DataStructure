38bd24b8fb5fb8f70067652c99198d71cd0fd34a
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2022-09-05 17:39:10 +0530

    Nested Priority queues Revise it

---
+++ Priority Queues/Find_k_Closest_element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int,int>>pq;
+        for(int e:arr){
+            pq.push({abs(e-x),e});
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+        vector<int>ans;
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        sort(ans.begin(),ans.end());
+        return ans ;
+    }
+};
