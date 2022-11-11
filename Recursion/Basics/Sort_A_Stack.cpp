04d2804bd66e4014cc2c8cf5d7792b5b91929592
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2022-11-11 22:34:33 +0530

    Create Sort_A_Stack.cpp

---
+++ Recursion/Basics/Sort_A_Stack.cpp
@@ -0,0 +1,77 @@
+https://practice.geeksforgeeks.org/problems/sort-a-stack/1
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+
+void printStack(stack<int> s)
+{
+    while (!s.empty())
+    {
+        printf("%d ", s.top());
+       	s.pop();
+    }
+    printf("\n");
+}
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+	SortedStack *ss = new SortedStack();
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	int k;
+	cin>>k;
+	ss->s.push(k);
+	}
+	ss->sort();
+	printStack(ss->s);
+}
+}
+// } Driver Code Ends
+
+
+/*The structure of the class is
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+*/
+
+/* The below method sorts the stack s 
+you are required to complete the below method */
+void insert(stack<int>&s , int temp){
+    if(s.empty() || temp>=s.top()){
+        s.push(temp);
+        return ;
+    }
+    
+    int k = s.top();
+    s.pop();
+    insert(s,temp);
+    s.push(k);
+    return ;
+    
+}
+void SortedStack :: sort()
+{
+   //Your code here
+   if(s.size()==1) return ;
+   int k = s.top();
+   s.pop();
+   sort();
+  insert(s,k);
+   
+   
+}
