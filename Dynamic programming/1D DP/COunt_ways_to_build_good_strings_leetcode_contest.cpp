ddb36e4574c297471e188b8d17bb987112d6d08f
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2022-11-15 01:28:47 +0530

    Update COunt_ways_to_build_good_strings_leetcode_contest.cpp

--- Dynamic programming/1D DP/COunt_ways_to_build_good_strings_leetcode_contest.cpp
+++ Dynamic programming/1D DP/COunt_ways_to_build_good_strings_leetcode_contest.cpp
@@ -4,11 +4,12 @@ class Solution {
 public:
      int mod = 1e9+7;
      vector<int>dp;
+     //this function will give count of  all the possible strings having length only equal to sz 
     int construct(int sz,int z,int o,vector<int>&dp){
-        if(sz==0) return 1;
-        if(sz<0) return 0;
-        if(dp[sz]!=-1) return dp[sz]%mod;
-        return dp[sz] = (construct(sz-o,z,o,dp)%mod+ construct(sz-z,z,o,dp)%mod)%mod;
+        if(sz==0) return 1; //Base Case :  If Size of string becomes exactly equal to sz , then sz will become 0 hence  return 1
+        if(sz<0) return 0; // other wise it will go in negative so return zero
+        if(dp[sz]!=-1) return dp[sz]%mod; //memoization
+        return dp[sz] = (construct(sz-o,z,o,dp)%mod+ construct(sz-z,z,o,dp)%mod)%mod; // checing for both cases for one and than for zeroes
         
         
     }
@@ -17,7 +18,7 @@ public:
         dp.resize(high+1,-1);
         for(int i = low ;i<=high ;++i){
             
-            ans = (ans + construct(i,zero,one,dp)%mod)%mod;
+            ans = (ans + construct(i,zero,one,dp)%mod)%mod;  // we are finding the all possiblities of length i from low to high
         }
         return ans;
     }
