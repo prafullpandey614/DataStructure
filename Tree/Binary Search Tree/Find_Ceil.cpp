6fb3d1876127cac44800ea03c42d26b6110938ae
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2022-12-03 02:54:26 +0530

    Create Find_Ceil.cpp

---
+++ Tree/Binary Search Tree/Find_Ceil.cpp
@@ -0,0 +1,25 @@
+void rec(Node*root,int d,int &c){
+    if(root->data == d){
+        c = root->data ;
+        return ;
+    }
+    if(root->data<d){
+        if(root->right)
+        rec(root->right,d,c);
+        return ;
+    }
+    if(root->data>d){
+        c = root->data ;
+        if(root->left)
+        rec(root->left,d,c);
+        return ;
+    }
+}
+int findCeil(Node* root, int input) {
+    if (root == NULL) return -1;
+    int c = -1;
+    rec(root,input,c);
+    return c; 
+    
+    // Your code here
+}
