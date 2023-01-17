402411c893b32aee13fb6c713f30cd3e693f4e37
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2023-01-18 01:20:44 +0530

    Most Important

--- Two Pointer Approach/count_the_number_of_good_subarrays.cpp
+++ Two Pointer Approach/count_the_number_of_good_subarrays.cpp
@@ -27,3 +27,32 @@ public:
         return res ;
     }
 };
+
+//second approach
+class Solution {
+public:
+    #define ll long long
+    long long countGood(vector<int>& nums, int k) {
+        ll ans = 0;
+        int n = nums.size();
+        unordered_map<ll,ll>mp;
+        ll l = 0 ;
+        ll res = 0 ;
+        ll cnt = 0 ;
+        for(int i = 0 ; i < n ; ++i){
+            mp[nums[i]]++;
+            while(l<n && cnt<k){
+                mp[nums[++l]]++;
+                if(mp[nums[l]]>1){
+                    cnt+=mp[nums[l]]-1;
+                }
+            }
+            int k = n-l+1;
+            cnt-=(mp[nums[i]]-1);
+            mp[nums[i]]--;
+            res+=k*1LL*(k-1)/2;
+        }
+       
+        return res ;
+    }
+};
