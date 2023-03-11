32eabde33ee24a5d7f5465ea67e77a3052d0a5d3
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2023-03-11 23:50:00 +0530

    Tried Once Again , Need to Observe to solve this

--- Recursion/Medium/Kth_SYMBOL_in_Grammer_observation_Question.cpp
+++ Recursion/Medium/Kth_SYMBOL_in_Grammer_observation_Question.cpp
@@ -15,3 +15,18 @@ public:
         return ans(n,k);
     }
 };
+
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        if(n==1) return 0;
+        int mid = ((1<<(n-1)))>>1 ;
+        if(k<=mid){
+            return kthGrammar(n-1,k);
+        }
+        else{
+            return 1-kthGrammar(n-1,k-mid); //toggling the result and making k inside mid , rest is same 
+        }
+
+    }
+};
