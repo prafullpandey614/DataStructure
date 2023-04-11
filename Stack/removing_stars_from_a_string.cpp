d85edbbeebbd23fcc4b84435490f968faa3902e5
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2023-04-11 23:53:03 +0530

    Create removing_stars_from_a_string.cpp

---
+++ Stack/removing_stars_from_a_string.cpp
@@ -0,0 +1,9 @@
+    string removeStars(string s) {
+        string res;
+        for (char c : s)
+            if (c == '*')
+                res.pop_back();
+            else
+                res += c;
+        return res;
+    }
