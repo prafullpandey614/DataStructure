4f18c605e6ddbd1a8a02f983b95aa7167804aff3
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2023-05-24 23:33:21 +0530

    Create Check_For_Subsequence.cpp

---
+++ Strings/Check_For_Subsequence.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std; 
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    bool isSubSequence(string A, string B) 
+    {
+        int i = 0;
+        int j = 0;
+        while(j<B.size()){
+            if(B[j++]==A[i]){
+                i++;
+            }
+        }
+        return i==A.size();
+        // code here
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        string A,B;
+        cin>>A;
+        cin>>B;  
+        Solution ob;
+        if(ob.isSubSequence(A,B))
+            cout<<"1"<<endl;
+        else
+            cout<<"0"<<endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
