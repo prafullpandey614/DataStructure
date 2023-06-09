770603774e1a7baa30c3a765e122711d1ef07111
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2023-06-09 23:43:35 +0530

    Create permutations_of-agiven_string.cpp

---
+++ Strings/permutations_of-agiven_string.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+		vector<string>find_permutation(string S)
+		{
+		   vector<string> res;
+    sort(S.begin(), S.end());
+    do
+    {
+      res.push_back(S);
+    } while (next_permutation(S.begin(), S.end()));
+    return res;
+		}
+};
+
+
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--)
+    {
+	    string S;
+	    cin >> S;
+	    Solution ob;
+	    vector<string> ans = ob.find_permutation(S);
+	    for(auto i: ans)
+	    {
+	    	cout<<i<<" ";
+	    }
+	    cout<<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
