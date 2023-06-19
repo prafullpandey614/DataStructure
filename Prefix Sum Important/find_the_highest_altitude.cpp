34de54a51eeba664aac720ceff641fb72a0695d7
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2023-06-19 23:56:23 +0530

    Create find_the_highest_altitude.cpp

---
+++ Prefix Sum Important/find_the_highest_altitude.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int ans = 0 ;
+        int sum = 0 ;
+        for(int i = 0 ;i <gain.size(); ++i){
+            sum+=gain[i];
+            ans = max(ans,sum);
+        }
+        return ans ;
+
+    }
+};
