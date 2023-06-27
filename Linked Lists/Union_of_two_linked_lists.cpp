9304618392bba514b1cdadc0f9bbcbf207ade85d
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2023-06-27 23:43:14 +0530

    Create Union_of_two_linked_lists.cpp

---
+++ Linked Lists/Union_of_two_linked_lists.cpp
@@ -0,0 +1,114 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+
+// } Driver Code Ends
+/*
+// structure of the node is as follows
+
+struct Node
+{
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+*/
+class Solution
+{
+   public:
+    struct Node* makeUnion(struct Node* head1, struct Node* head2)
+    {
+        // code here
+        set<int>s;
+        Node *curr=head1;
+        while(curr!=NULL){
+            s.insert(curr->data);
+            curr=curr->next;
+        }
+        curr=head2;
+        while(curr!=NULL){
+            s.insert(curr->data);
+            curr=curr->next;
+        }
+        Node* temp=new Node(-1);
+        Node* head3=temp;
+        for(auto it:s){
+            temp->next=new Node(it);
+            temp=temp->next;
+        }
+        return head3->next;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+struct Node* buildList(int size)
+{
+    int val;
+    cin>> val;
+    
+    Node* head = new Node(val);
+    Node* tail = head;
+    
+    for(int i=0; i<size-1; i++)
+    {
+        cin>> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        
+        cin>>n;
+        Node* first = buildList(n);
+        
+        cin>>m;
+        Node* second = buildList(m);
+        Solution obj;
+        Node* head = obj.makeUnion(first,second);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
