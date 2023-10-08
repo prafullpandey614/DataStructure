9a74e12e3fc2ae5e2b8754eee8e1099cb49f5226
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2023-10-08 23:49:13 +0530

    Create Minimum_processing_time.cpp

---
+++ Sorting/Minimum_processing_time.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minProcessingTime(vector<int>& p, vector<int>& tasks) {
+        sort(tasks.begin(),tasks.end()) ;
+        sort(p.begin(),p.end(),greater<int>());
+        // for(int x:p) cout<<x<<" ";
+        // cout<<endl;
+        // for(int x:tasks) cout<<x<<" ";
+        // cout<<endl;
+        
+        int i = 3 ;
+        int ans = 0 ;
+        for(int  x: p ){
+            ans = max(ans,x+tasks[i]);
+            i+=4;
+        }
+        return ans ;
+        
+        
+        // return 1;
+        return max(tasks[tasks.size()-1]+p[p.size()-1],tasks[3]+p[0]);
+    }
+};
