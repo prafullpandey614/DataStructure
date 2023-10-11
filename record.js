12432b95076900f8c1c5671d1c68ad1e9dc1c638
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2023-10-11 23:58:47 +0530

    Update record.js

--- record.js
+++ record.js
@@ -2,7 +2,7 @@
         recordButton = event;
         
         msb = document.querySelector("#user_message")
-        loader = document.getElementById("loader");
+        // loader = document.getElementById("loader");
         container = document.getElementById("container");
 
         if (event.textContent == "Record") {
