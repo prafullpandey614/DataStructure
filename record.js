2cc13a3b086961e944107841869a572114fc48c6
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2023-10-13 23:59:13 +0530

    Update record.js

--- record.js
+++ record.js
@@ -2,15 +2,16 @@
         recordButton = event;
         
         msb = document.querySelector("#user_message")
-        // loader = document.getElementById("loader");
-        container = document.getElementById("container");
+        // // loader = document.getElementById("loader");
+        // container = document.getElementById("container");
 
-        if (event.textContent == "Record") {
-          console.log('start');
-          startRecording();
-        } else {
-          console.log('end');
-         stopRecording();  
+        // if (event.textContent == "Record") {
+        //   console.log('start');
+        //   startRecording();
+        // } else {
+        //   console.log('end');
+        //  stopRecording();  
 
-        } 
+        // } 
+  //abandoned
       }
