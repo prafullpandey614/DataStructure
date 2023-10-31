87a633cb7d864bc147cd29b9be696c37c34f3b0f
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2023-11-01 03:08:43 +0530

    Create find_the_original_array_of_prefix_xor.cpp

---
+++ Subarrays/Xor/find_the_original_array_of_prefix_xor.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+          vector<int>result ; 
+          result.push_back(pref[0]);
+          for(int i =1; i<pref.size(); ++i){
+              result.push_back(pref[i-1]^pref[i]);
+          }
+          return result ;
+    }
+};
